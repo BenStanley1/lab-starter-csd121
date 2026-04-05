701c99439b22d89d055f76be59f8693055b40cb6
benstanley <25013977@saultcollege.ca>
2026-04-04 22:08:22 -0400

    Lab 6 progress, 1.11

--- src/main/java/part1/Main.java
+++ src/main/java/part1/Main.java
@@ -3,22 +3,22 @@ package part1;
 import part1.logging.*;
 import part1.logging.LogLevel;
 import part1.util.Messages;
-
+import com.diogonunes.jcolor.Ansi;
+import com.diogonunes.jcolor.Attribute;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.time.Instant;
 import java.util.List;
 
 public class Main {
 
     static void main(String[] args) {
 
+        Logger logger = new CensoriousLogger(List.of("hungrish", "drabblex", "clorphed", "snorvish", "grumblet", "flonky", "blarfish"));
+
         try {
 
-            /* === Easy loggers === */
-            // TODO: Implementnt AT LEAST one of these
-//            Logger logger = new ColorLogger();
-//            Logger logger = new InDenialLogger();
-//            Logger logger = new LoudLogger();
+
 
             /* === Medium loggers === */
             // TODO: Implement at least one of these
@@ -35,7 +35,7 @@ public class Main {
 
             // TODO: you can change the first argument here to one of the loggers above,
             //       or to a different constructor call based on the examples above.
-            run(new ConsoleLogger(), 50);
+            run(new CensoriousLogger((List.of("hungrish", "drabblex", "clorphed", "snorvish", "grumblet", "flonky", "blarfish"))),50);
 
             // TODO: uncomment this while you are trying out your MemoryLogger
             // export(logger, new FileOutputStream("logs.txt"));  // OR try System.out as the second parameter!
@@ -46,15 +46,13 @@ public class Main {
         }
     }
 
-    public static void run(ConsoleLogger logger, int n) {
-        for ( int i = 0; i < n; i++ ) {
+    public static void run(Logger logger, int n) {
+        for (int i = 0; i < n; i++) {
             var randomLevel = LogLevel.values()[(int) (Math.random() * LogLevel.values().length)];
             var message = Messages.getRandomMessage();
             logger.log(message, randomLevel);
         }
     }
 
-    public static void export(Exportable exporter, OutputStream stream) {
-        exporter.exportTo(stream);
-    }
+
 }
